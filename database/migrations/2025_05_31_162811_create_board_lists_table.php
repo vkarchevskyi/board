bb16e6d5e8944ac51a892fde016a72fb51af79a8
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-05-31 21:02:12 +0300

    feat(board): create boards lists table

---
+++ database/migrations/2025_05_31_162811_create_board_lists_table.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('board_lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->foreignId('board_id')->constrained('boards')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('board_lists');
+    }
+};
