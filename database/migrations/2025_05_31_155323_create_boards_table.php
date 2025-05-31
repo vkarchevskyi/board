91db3b82b16aa065cd86ec14948ef035aa83becd
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-05-31 21:02:02 +0300

    feat(board): create boards table

---
+++ database/migrations/2025_05_31_155323_create_boards_table.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->text('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('boards');
+    }
+};
